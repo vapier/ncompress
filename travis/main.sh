446d935ef12192ac73d4d98e7a0e155d96c9fb41
Mike Frysinger <vapier@gmail.com>
2019-01-09 22:23:29 -0500

    travis: add support for Windows

--- travis/main.sh
+++ travis/main.sh
@@ -2,9 +2,26 @@
 
 . "${0%/*}"/lib.sh
 
-main() {
+build_make() {
 	m
 	m check
 	m install DESTDIR="${PWD}/root"
+}
+
+build_linux() { build_make; }
+
+build_osx() { build_make; }
+
+build_windows() {
+	v ${CC} -o compress \
+		-Wall \
+		-DNOFUNCDEF -DUSERMEM=800000 -DREGISTERS=3 \
+		compress42.c
+	# The tests currently fail on Windows.  No idea why.
+	./tests/runtests.sh || :
+}
+
+main() {
+	"build_${TRAVIS_OS_NAME}"
 }
 main "$@"

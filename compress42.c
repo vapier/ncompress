4e199362505c193fefe89114d2e237cdc1cbec69
Mike Frysinger <vapier@gmail.com>
2019-01-09 22:18:01 -0500

    disable unistd.h include on DOS & WINDOWS

--- compress42.c
+++ compress42.c
@@ -139,13 +139,16 @@
 #include	<stdio.h>
 #include	<stdlib.h>
 #include	<string.h>
-#include	<unistd.h>
 #include	<fcntl.h>
 #include	<ctype.h>
 #include	<signal.h>
 #include	<sys/types.h>
 #include	<sys/stat.h>
 #include	<errno.h>
+
+#if !defined(DOS) && !defined(WINDOWS)
+#	include	<unistd.h>
+#endif
 
 #ifdef DIRENT
 #	include	<dirent.h>
